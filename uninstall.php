ebea40ee17112090fc535affe62f832b59274532
ivankk <ivankk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-15 15:30:22 +0000

    1.0 release tagged
    git-svn-id: https://plugins.svn.wordpress.org/brightcove-video-connect/tags/1.0@1181185 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Uninstall Brightcove
+ *
+ * Removes all Brightcove data stored by the plugin
+ *
+ * @since   1.0.0
+ *
+ * @package Brightcove_Video_Cloud
+ */
+
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) || ! WP_UNINSTALL_PLUGIN ) {
+	exit();
+}
+
+global $wpdb;
+
+//Delete static options
+delete_option( '_brightcove_pending_videos' );
+delete_option( '_brightcove_salt' );
+delete_option( '_brightcove_accounts' );
+delete_option( '_brightcove_default_account' );
+
+//Delete synced video data
+$wpdb->query( "DELETE FROM $wpdb->postmeta WHERE meta_key LIKE '_brightcove%';" );
+$wpdb->query( "DELETE FROM $wpdb->posts WHERE post_type = 'brightcove-playlist';" );
+$wpdb->query( "DELETE FROM $wpdb->posts WHERE post_type = 'brightcove-video';" );
+
+//Delete variable options
+$wpdb->query( "DELETE FROM $wpdb->options WHERE option_name LIKE '_brightcove%';" );
+$wpdb->query( "DELETE FROM $wpdb->options WHERE option_name LIKE '_bc_player%';" );
+$wpdb->query( "DELETE FROM $wpdb->options WHERE option_name LIKE '_notifications_subscribed_%';" );
